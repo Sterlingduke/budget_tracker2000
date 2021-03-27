15302ab3ac7a4b35147e6889e35df375957e5ebc
Sterling <sterlingcduke@gmail.com>
2021-03-27 09:10:10 -0500

    deleted icons

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000 || process.env.PORT;
+// const PORT = 3000 || process.env.PORT;
+const PORT = 3000
 
 const app = express();
 
